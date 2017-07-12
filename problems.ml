fda0fc15e78488f5393dab77c30a4090f526d8bf
Rachel Ehrlich <rae84@cornell.edu>
2017-07-11 22:14:25 -0400

    problem 8

--- problems.ml
+++ problems.ml
@@ -46,6 +46,42 @@ let problem_6 n =
   (sum * sum) - squares_sum
 ;;
 
+let problem_8 n =
+  let s =
+    "73167176531330624919225119674426574742355349194934
+      96983520312774506326239578318016984801869478851843
+  85861560789112949495459501737958331952853208805511
+  12540698747158523863050715693290963295227443043557
+  66896648950445244523161731856403098711121722383113
+  62229893423380308135336276614282806444486645238749
+  30358907296290491560440772390713810515859307960866
+  70172427121883998797908792274921901699720888093776
+  65727333001053367881220235421809751254540594752243
+  52584907711670556013604839586446706324415722155397
+  53697817977846174064955149290862569321978468622482
+  83972241375657056057490261407972968652414535100474
+  82166370484403199890008895243450658541227588666881
+  16427171479924442928230863465674813919123162824586
+  17866458359124566529476545682848912883142607690042
+  24219022671055626321111109370544217506941658960408
+  07198403850962455444362981230987879927244284909188
+  84580156166097919133875499200524063689912560717606
+  05886116467109405077541002256983155200055935729725
+  71636269561882670428252483600823257530420752963450"
+  in
+  let nums =
+    String.to_list s
+  |> List.filter_map ~f:Char.get_digit
+  in
+  let product l =
+    List.fold l ~init:1 ~f:(fun acum n -> acum * n)
+  in
+  List.fold (List.range 0 ((List.length nums)-n+1)) ~init:0 ~f:(fun max i ->
+      let result = product (List.slice nums i (i+n)) in
+      Int.max max result
+  )
+;;
+
 let problem_14 () = 
   let rec collatz values n =
     match Int.Map.find values n with
@@ -98,8 +134,10 @@ let () =
   (* Out_channel.output_string stdout ("Problem 2: " ^ (Int.to_string problem_2)); *)
   (* let problem_6 = problem_6 100 in *)
   (* Out_channel.output_string stdout ("Problem 6: " ^ (Int.to_string problem_6)); *)
+  let problem_8 = problem_8 13 in
+  Out_channel.output_string stdout ("Problem 8: " ^ (Int.to_string problem_8));
   (* let problem_14 = problem_14 () in *)
   (* Out_channel.output_string stdout ("Problem 14: " ^ (Int.to_string problem_14)); *)
-  let problem_15 = problem_15 () in
-  Out_channel.output_string stdout ("Problem 15: " ^ (Int.to_string problem_15));
+  (* let problem_15 = problem_15 () in *)
+  (* Out_channel.output_string stdout ("Problem 15: " ^ (Int.to_string problem_15)); *)
 ;;
